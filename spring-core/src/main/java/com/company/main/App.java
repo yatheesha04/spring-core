249f53491602acb4e2f3021dc6b93b16a1527571
yatheesha04 <42319204+yatheesha04@users.noreply.github.com>
2020-07-28 14:58:12 +0530

    xml based configaration

--- spring-core/src/main/java/com/company/main/App.java
+++ spring-core/src/main/java/com/company/main/App.java
@@ -10,7 +10,7 @@ public class App {
 	public static void main(String[] args) {
 		
 		ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
-		Vehicle obj = (Vehicle)context.getBean("vehicle");
+		Vehicle obj = (Vehicle)context.getBean("bike");
 	       obj.drive();
 	}
 
